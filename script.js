8fc68bb2ecd30bd0eff2726ffcb7f6ef3bbe497d
Jethro Dean <jethroldean@gmail.com>
2022-04-19 12:52:40 +1000

    Completed, uploading code from CodePen

---
+++ script.js
@@ -0,0 +1,9 @@
+const easterEggImage = document.getElementById("image");
+const easterEggOutcome = document.getElementById("main");
+
+easterEggImage.addEventListener("click", function() {
+  
+  easterEggOutcome.style.backgroundColor = "#ada9e4"
+  
+  alert("You found the easter egg!")
+})
